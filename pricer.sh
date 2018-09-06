ae5cc71ca1045224bdb768079cb573a3087702f9
Elim <elim723@gmail.com>
2018-09-05 23:30:08 -0400

    98% done

--- pricer.sh
+++ pricer.sh
@@ -1,5 +1,22 @@
 #!/bin/bash 
 
+####
+#### By Elim Thompson (09/04/2018)
+####
+#### This bash script is served as a test
+#### procedure for the correctness.
+####
+#### For each target size (1, 200, 10000), 
+#### I run the pricer.py with the corresponding
+#### input files. Then I compare my output
+#### file to the answer provided by Kinetica.
+#### It then print out the differences between
+#### my output and the provided answer. If
+#### my output is correct, the count should
+#### by 0.
+####
+#######################################################
+
 ### loop through targetsizes of 1, 200, 10000
 for ts in 1 200 10000; do
     ## print output to Pricer/elim1(or 200 or 10000).out
